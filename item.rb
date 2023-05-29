5e16cce0c71b5c0c4b95c2c92f1238fbae3fa2c3
stephenkati <stephenkatuli88@gmail.com>
2023-05-29 17:03:09 +0300

    create item.rb

---
+++ item.rb
@@ -0,0 +1,13 @@
+class Item
+  attr_accessor :genre, :author, :source, :label
+
+  def initialize(publish_date, archived: false)
+    @id = rand(1...1000)
+    @publish_date = publish_date
+    @archived = archived
+  end
+
+  def can_be_archived?; end
+
+  def move_to_archive; end
+end
