d78f900a299acd829a6d42c1f7b1d57d5e1185fc
stephenkati <stephenkatuli88@gmail.com>
2023-05-30 09:37:11 +0300

    update relationshps

--- item.rb
+++ item.rb
@@ -10,4 +10,24 @@ class Item
   def can_be_archived?; end
 
   def move_to_archive; end
+
+  def genre(genre)
+    @genre = genre
+    genre.add_item(self) unless genre.items.includes?(self)
+  end
+
+  def author(author)
+    @author = author
+    author.add_item(self) unless author.items.includes?(self)
+  end
+
+  def source(source)
+    @source = source
+    source.add_item(self) unless source.items.includes?(self)
+  end
+
+  def label(label)
+    @label = label
+    label.add_item(self) unless label.items.includes?(self)
+  end
 end
