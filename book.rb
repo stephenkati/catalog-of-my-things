13abe40de2621b53b1376aaad1790f660900efa8
stephenkati <stephenkatuli88@gmail.com>
2023-05-30 15:17:40 +0300

    implement methods

--- book.rb
+++ book.rb
@@ -1,8 +1,21 @@
 require_relative 'item'
 
 class Book < Item
-  def initialize(publisher, cover_state)
+  attr_reader :id
+  attr_accessor :publisher, :cover_state
+
+  def initialize(publisher, cover_state, publish_date, archived: false)
+    super(publish_date, archived: false)
+    @id = rand(1...1000)
     @publisher = publisher
     @cover_state = cover_state
+  end
+
+  def can_be_archived?
+    if @cover_state === 'bad' || super
+      return archived = true
+    else
+      return archived = false
+    end
   end
 end
--- label.rb
+++ label.rb
@@ -1,10 +1,18 @@
 require_relative 'item'
 
 class Label < Item
-def initialize(title, color)
-  @id = rand(1...1000)
-  @title = title
-  @color = color
-  @items = []
-end
+  attr_reader :id
+  attr_accessor :title, :color, :items
+
+  def initialize(title, color)
+    @id = rand(1...1000)
+    @title = title
+    @color = color
+    @items = []
+  end
+
+  def add_item(item)
+    @items << item
+    item.label = self
+  end
 end
