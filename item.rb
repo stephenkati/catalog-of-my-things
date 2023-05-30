395cc13eb0dcd2f0fb523ce3c733e36868526f92
stephenkati <stephenkatuli88@gmail.com>
2023-05-30 17:14:04 +0300

    test Item class

--- item.rb
+++ item.rb
@@ -1,6 +1,8 @@
+require 'date'
+
 class Item
   attr_reader :id
-  attr_accessor :publish_date, :archived, :genres, :authors, :sources, :labels
+  attr_accessor :publish_date, :archived, :genres, :authors, :sources, :labels, :label
 
   def initialize(publish_date, archived: false)
     @id = rand(1...1000)
@@ -13,32 +15,12 @@ class Item
   end
 
   def can_be_archived?
-    Time.now.year - Date.parse(@publish_date.year) > 10
+    Time.now.year - Date.parse(@publish_date).year > 10
   end
 
   def move_to_archive
     return unless can_be_archived?
 
     @archived = true
-  end
-
-  def add_genre(genre)
-    genres << genre
-    genre.add_item(self)
-  end
-
-  def add_author(author)
-    authors << author
-    author.add_item(self)
-  end
-
-  def add_source(source)
-    sources << source
-    source.add_item(self)
-  end
-
-  def add_label(label)
-    labels << label
-    label.add_item(self)
   end
 end
