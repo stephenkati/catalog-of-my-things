4a95441d313856d474e14fc8e65c2b9990a7a53c
stephenkati <stephenkatuli88@gmail.com>
2023-05-31 11:39:20 +0300

    create genre class

---
+++ classes/genre.rb
@@ -0,0 +1,17 @@
+require './item'
+
+class Genre < Item
+  attr_accessor :name, :items
+  attr_reader :id
+
+  def initialize(name)
+    @id = rand(1...1000)
+    @name = name
+    @items = []
+  end
+
+  def add_item(item)
+    @items << item
+    genre.item = self
+  end
+end
