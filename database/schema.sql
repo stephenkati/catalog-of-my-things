b1ac9e09c983e2ce06cc6cbe508034070add1e52
stephenkati <stephenkatuli88@gmail.com>
2023-05-31 16:41:11 +0300

    add genre, albums

--- database/schema.sql
+++ database/schema.sql
@@ -14,3 +14,16 @@ CREATE TABLE label(
   title VARCHAR(100),
   color VARCHAR(100)
 );
+
+CREATE TABLE music_album(
+  id INT SERIAL PRIMARY KEY,
+  on_spotify BOOLEAN,
+  publish_date DATE,
+  archive BOOLEAN,
+  genre_id INT REFERENCES genre(id)
+);
+
+CREATE TABLE genre(
+  id INT SERIAL PRIMARY KEY,
+  name VARCHAR(100)
+);
